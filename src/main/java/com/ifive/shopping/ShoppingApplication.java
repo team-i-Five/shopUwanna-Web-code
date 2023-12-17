da80c46c1636dbfd878ff880c47eca361139bdbc
shims94 <shims94@gmail.com>
2023-12-17 20:18:01 +0900

    fix database error

--- src/main/java/com/ifive/shopping/ShoppingApplication.java
+++ src/main/java/com/ifive/shopping/ShoppingApplication.java
@@ -2,8 +2,10 @@ package com.ifive.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// 현재 DB 정보가없어서
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class}) 
 public class ShoppingApplication {
 
 	public static void main(String[] args) {
